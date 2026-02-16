0e839876fb5759ff6599315d88c9576b54b1a0b3
Rajendran Nadar <17236768+raajnadar@users.noreply.github.com>
2026-02-17 01:19:53 +0530

    chore: fix react native web example not loading

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,20 +1,26 @@
-const path = require("path");
-const fs = require("fs");
-const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path')
+const fs = require('fs')
+const { getDefaultConfig } = require('expo/metro-config')
 
-const config = getDefaultConfig(__dirname);
-const workspaceRoot = path.resolve(__dirname, "..");
+const config = getDefaultConfig(__dirname)
+const workspaceRoot = path.resolve(__dirname, '..')
 const stableEmptyModulePath = path.join(
   workspaceRoot,
-  "node_modules",
-  "metro-runtime",
-  "src",
-  "modules",
-  "empty-module.js"
-);
+  'node_modules',
+  'metro-runtime',
+  'src',
+  'modules',
+  'empty-module.js',
+)
 
 if (fs.existsSync(stableEmptyModulePath)) {
-  config.resolver.emptyModulePath = stableEmptyModulePath;
+  config.resolver.emptyModulePath = stableEmptyModulePath
 }
 
-module.exports = config;
+config.watchFolders = [workspaceRoot]
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+]
+
+module.exports = config
